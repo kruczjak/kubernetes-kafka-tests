0e9a1ce9ec506fbdcea0c99fdcf6612df43f12b9
kruczjak <kruczjak@gmail.com>
2018-05-26 21:44:42 +0200

    Updates to build.sh

--- kafka-app/build.sh
+++ kafka-app/build.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+set -e
+
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
-docker build -t example-app .
+docker build -t kruczjak/kafka-example-app .
+docker push kruczjak/kafka-example-app
+rm main
